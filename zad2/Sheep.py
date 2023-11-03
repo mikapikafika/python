92fa8fb8b1065a55dbb5e002979ea90542814985
JStepnicki <j.stepnicki2002@gmail.com>
2023-11-03 15:28:47 +0100

    sheep can move

--- zad2/Sheep.py
+++ zad2/Sheep.py
@@ -1,2 +1,12 @@
+from random import choice
+
 class Sheep:
     position = (0, 0)
+    move_distance = 0.5
+    directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+    def move(self):
+        direction = choice(self.directions)
+        new_x = self.position[0] + (direction[0] * self.move_distance)
+        new_y = self.position[1] + (direction[1] * self.move_distance)
+        self.position = (new_x, new_y)
---
+++ zad2/Wolf.py
@@ -0,0 +1,3 @@
+class Wolf:
+    position = (0, 0)
+    move_distance = 1
