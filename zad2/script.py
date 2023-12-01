5878d7eecd96594d49769109e54e5a4b64b033f8
JStepnicki <j.stepnicki2002@gmail.com>
2023-12-01 18:50:26 +0100

    help added

--- zad2/script.py
+++ zad2/script.py
@@ -5,28 +5,40 @@ import logging
 from Game import Game
 from Meadow import Meadow
 
-
-parser = argparse.ArgumentParser(description='Process some integers.')
+start = True
+parser = argparse.ArgumentParser(description='Herzlich willkommen', add_help=False)
 
 parser.add_argument('-c', '--config', metavar='FILE', type=Path)
-# parser.add_argument('-h', '--help')
+parser.add_argument('-h', '--help', action='store_true')
 parser.add_argument('-l', '--log', metavar='LEVEL', type=int)
 parser.add_argument('-r', '--rounds', metavar='NUM ', type=int)
 parser.add_argument('-s', '--sheep', metavar='NUM ', type=int)
 parser.add_argument('-w', '--wait', action='store_true')
 
 args = parser.parse_args()
-
-if args.log is not None:
-    with open('chasing.log', 'w') as file:
-        file.write("test")
-        file.close()
-
 if args.config is not None:
     with open(args.config, 'r') as file:
         if file is None:
             raise 'File not found'
         print(file.read())
+        file.close()
+
+if args.help:
+    print("This program simulates a wolf chasing sheep in a meadow.\n")
+    print("The program takes the following arguments:")
+    print("-c, --conf FILE where FILE is a path to a configuration file. ")
+    print("-h, --help to display this help message.")
+    print("-l, --log LEVEL where LEVEL is a logging level.")
+    print("-r, --rounds NUM where NUM is a number of rounds that will be "
+          "played. ")
+    print("-s, --sheep NUM where NUM is a number of sheep that will be "
+          "placed in the meadow. ")
+    print("-w, --wait to wait for pressing any key after each round.\n")
+    start = False
+
+if args.log is not None:
+    with open('chasing.log', 'w') as file:
+        file.write("test")
         file.close()
 
 if args.rounds is not None:
@@ -42,6 +54,7 @@ if args.sheep is not None:
         Meadow.sheepQuantity = args.sheep
 if args.wait:
     Game.break_after_round = True
-gra = Game()
-gra.run()
 
+if start:
+    gra = Game()
+    gra.run()
