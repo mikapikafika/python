0262079096650127307cb31838fae4fc2c02cc00
mwysocka <mikafikapika@gmail.com>
2024-01-17 02:40:30 +0100

    Removed Decimal Points

--- webapp/data_app/forms.py
+++ webapp/data_app/forms.py
@@ -3,9 +3,6 @@ from .models import DataPoint
 
 
 class DataPointForm(forms.ModelForm):
-    feature1 = forms.DecimalField(max_digits=6, decimal_places=2)
-    feature2 = forms.DecimalField(max_digits=6, decimal_places=2)
-
     class Meta:
         model = DataPoint
         fields = ['feature1', 'feature2', 'category']
